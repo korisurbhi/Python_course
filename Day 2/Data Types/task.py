cdeb27db418cacd97baa9044932c1f500e6d659f
korisurbhi <surbhikori11096@gmail.com>
2025-08-23 22:29:55 +0530

    python

---
+++ Day 2/Data Types/task.py
@@ -0,0 +1,19 @@
+#subscripting
+print("Hello"[0])
+print("Hello"[-1])
+
+#string
+print("123" +"123")
+
+#integer = whole number
+print(123 + 123)
+
+#large integers
+print(123_456_789)
+
+#float = floating point number
+print(3.145)
+
+#boolean
+print(True)
+print(False)
---
+++ Day 2/Number Manipulation/task.py
@@ -0,0 +1,22 @@
+bmi = 84 / 1.65 ** 2
+print(bmi)
+
+print(int(bmi))
+print(round(bmi))
+print(round(bmi,2))
+print("\n")
+score = 0
+
+#User scores a point
+score +=1
+
+#f-strings
+print(f"Your score is" +str(score))
+
+print("\n\n")
+
+score = 0
+height = 1.8
+is_winning = True
+
+print(f"Your score is {score}, your height is {height}. you are winning is {is_winning}")
