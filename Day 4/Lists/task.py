0a377c9830d6905e0507a0dae108d3ac84b82fa2
korisurbhi <surbhikori11096@gmail.com>
2025-08-29 18:31:01 +0530

    python

---
+++ Day 4/Lists/task.py
@@ -0,0 +1,5 @@
+city_of_maharastra = ["mumbai","kolhapur","satara","kokan","ratnagiri","pune"]
+city_of_maharastra[1]= "nashik"
+print(city_of_maharastra)
+city_of_maharastra.append("alibaug")
+print(city_of_maharastra)
