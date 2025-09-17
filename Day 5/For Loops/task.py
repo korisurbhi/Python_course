c97f131a1148bcb3d0658d1bb597cc0356c7f13c
korisurbhi <surbhikori11096@gmail.com>
2025-09-17 22:03:04 +0530

    python

---
+++ Day 5/For Loops/task.py
@@ -0,0 +1,8 @@
+fruits = ["Apple", "Peach", "Pear"]
+name= ["surbhi","sanika","prachiti","neha"]
+for fruit in fruits:
+    print(fruit)
+    print(fruit,"This is fruit")
+print("They are my closet friend")
+for names in name:
+    print(names)
---
+++ Day 5/Highest Score/task.py
@@ -0,0 +1,26 @@
+student_scores = [150, 142, 185, 120, 171, 184, 149, 24, 59, 68, 199, 78, 65, 89, 86, 55, 91, 64, 89]
+print("Two type of sum")
+
+print("1st type of already sum is function")
+total_exam_score = sum(student_scores)
+print(total_exam_score)
+
+print("2nd type we made sum function")
+sum = 0
+for score in student_scores:
+    sum+=score
+print(sum)
+
+
+# print(range(1, 10))
+
+print("there are also two types")
+
+print("1st type of already max is function")
+print(max(student_scores))
+max_score = 0
+print("2nd type we made max function")
+for score in student_scores:
+    if score > max_score:
+        max_score = score
+print(max_score)
